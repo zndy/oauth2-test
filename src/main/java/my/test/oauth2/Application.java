2182d19576dd022694f54f665a328c3c36cd87d7
z.xin <z.xin@WKS-XIN>
2019-04-24 09:26:08 +0200

    adds link

--- src/main/java/my/test/oauth2/Application.java
+++ src/main/java/my/test/oauth2/Application.java
@@ -3,6 +3,10 @@ package my.test.oauth2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * https://blog.csdn.net/u013815546/article/details/76898524
+ * without redis
+ */
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
